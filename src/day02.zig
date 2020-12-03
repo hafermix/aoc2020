14261a8adf6c61bcb99daa4936fce3157a118e0a
hafermix <hafermix@gmail.com>
2020-12-03 19:23:36 +0100

    Day 03: Done

--- src/day02.zig
+++ src/day02.zig
@@ -1,80 +1,8 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
-
-const Entry = struct {
-    policy: struct {
-        least: u16,
-        most: u16,
-        letter: u8,
-    } = undefined,
-    password: []u8 = undefined,
-};
+const print = std.debug.print;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true, .enable_memory_limit = true }){};
-    const allocator = &gpa.allocator;
-    defer {
-        const bytesUsed = gpa.total_requested_bytes;
-        const info = gpa.deinit();
-        std.log.info("\n\t[*] Leaked: {}\n\t[*] Bytes leaked: {}", .{ info, bytesUsed });
-    }
 
-    var args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    const input_file = try std.fs.cwd().openFile(args[1], .{});
-    defer input_file.close();
-    const input_reader = input_file.reader();
-
-    var entry_list = std.ArrayList(Entry).init(allocator);
-    defer {
-        for (entry_list.items) |item| {
-            allocator.destroy(item.password.ptr);
-        }
-        entry_list.deinit();
-    }
-
-    var buf: [512]u8 = undefined;
-    while (try input_reader.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
-        var entry = Entry{};
-        var iter = std.mem.tokenize(line, " -");
-
-        entry.policy.least = try std.fmt.parseInt(@TypeOf(entry.policy.least), iter.next().?, 10);
-        entry.policy.most = try std.fmt.parseInt(@TypeOf(entry.policy.most), iter.next().?, 10);
-        entry.policy.letter = iter.next().?[0];
-        
-        entry.password = try allocator.dupe(u8, iter.next().?);
-        try entry_list.append(entry);
-    }
-    
-    std.log.info("Solution to part 1 is {}.", .{part1(allocator, entry_list)});
-    std.log.info("Solution to part 2 is {}.", .{part2(allocator, entry_list)});
-}
-
-fn part1(allocator: *std.mem.Allocator, entry_list: std.ArrayList(Entry)) u32 {
-    var valid: u32 = 0;
-    for (entry_list.items) |entry| {
-        var count: u32 = 0;
-        for (entry.password) |c| {
-            if (c == entry.policy.letter) {
-                count += 1;
-            }
-        }
-
-        if (count >= entry.policy.least and count <= entry.policy.most) {
-            valid += 1;
-        }
-    }
-    return valid;
-}
-
-fn part2(allocator: *std.mem.Allocator, entry_list: std.ArrayList(Entry)) u32 {
-    var valid: u32 = 0;
-    for (entry_list.items) |entry| {
-        if ((entry.password[entry.policy.least - 1] == entry.policy.letter) !=
-            (entry.password[entry.policy.most - 1] == entry.policy.letter)) {
-            valid += 1;
-        }
-    }
-    return valid;
 }
--- src/day03.zig
+++ src/day03.zig
@@ -3,6 +3,97 @@ const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const print = std.debug.print;
 
-pub fn main() !void {
+const Row = []bool;
+const open = '.';
+const tree = '#';
+const Slope = struct { right: u16, down: u16 = 1 };
 
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = &gpa.allocator;
+    defer _ = gpa.deinit();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len != 2) {
+        std.log.err("Please provide an input file path.", .{});
+        return;
+    }
+
+    const input_file = try std.fs.cwd().openFile(args[1], .{});
+    defer input_file.close();
+    const input_reader = input_file.reader();
+
+    var forest = std.ArrayList(Row).init(allocator);
+    defer {
+        for (forest.items) |item| {
+            allocator.destroy(item.ptr);
+        }
+        forest.deinit();
+    }
+
+    var buf: [64]u8 = undefined;
+    var row_width: u16 = 0;
+    while (try input_reader.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
+        if (row_width == 0) {
+            row_width = @intCast(@TypeOf(row_width), line.len);
+        }
+        
+        const row: Row = try allocator.alloc(bool, row_width);
+
+        var pos: u16 = 0;
+        while (pos < row_width) {
+            if (line[pos] == open) {
+                row[pos] = false;
+            } else {
+                row[pos] = true;
+            }
+            pos += 1;
+        }
+
+        try forest.append(row);
+    }
+
+    std.log.info("The solution for part 1 is {}.", .{part1(forest)});
+
+    const slopes = [_]Slope{
+        .{ .right = 1 },
+        .{ .right = 3 },
+        .{ .right = 5 },
+        .{ .right = 7 },
+        .{ .right = 1, .down = 2 },
+    };
+    var solution2: u32 = 1;
+    for (slopes) |slope| {
+        solution2 *= part2(forest, slope);
+    }
+    std.log.info("The solution for part 2 is {}.", .{solution2});
+}
+
+fn part1(forest: std.ArrayList(Row)) u32 {
+    return part2(forest, Slope{ .right = 3});
+}
+
+fn part2(forest: std.ArrayList(Row), slope: Slope) u32 {
+    var row_width = forest.items[0].len;
+    var column_index: u16 = 0;
+    var row_index: u16 = 0;
+    
+    var hits: u32 = 0;
+    for (forest.items) |row| {
+        if (@mod(row_index, slope.down) != 0) {
+            row_index += 1;
+            continue;
+        }
+
+        if (row[column_index] == true) {
+            hits += 1;
+        }
+
+        column_index = @mod(column_index + slope.right,
+            @intCast(@TypeOf(column_index), row_width));
+        row_index += 1;
+    }
+    return hits;
 }
